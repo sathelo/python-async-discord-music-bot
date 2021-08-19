2667781a2ea6507f0836ba5f47f46889b152d7a4
sathelo <sashenkocasha@mail.ru>
2021-08-19 17:23:06 +0300

    изменил хранение токена

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import discord
-from discord.ext import commands
-from discord.flags import Intents
-
 import music_configuration
-from settings import TOKEN
+from discord.ext import commands
+from dotenv import load_dotenv
+from os import environ
 
 
+load_dotenv()
 cogs = [music_configuration]
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
@@ -19,4 +19,5 @@ async def bots(ctx):
     await ctx.send('На месте! ✅')
 
 
-bot.run(TOKEN)
+# Get token
+bot.run(environ['TOKEN'])
--- settings.py
+++
@@ -1,2 +0,0 @@
-TOKEN = 'ODc3MjQzNjIxMjk5MDY0OTAy.YRvyyQ.46foiOG_5zfdH8_hPF0DLikuhS4'
-# C:\normalfaggot\utils
