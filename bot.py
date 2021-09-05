b428d03613f5dc98146bbabdef507ed070cf6697
sathelo <sashenkocasha@mail.ru>
2021-09-05 23:32:11 +0300

    Статус бота

--- bot.py
+++ bot.py
@@ -1,22 +1,25 @@
 import discord
 import music_configuration
 import about_configuration
+from discord.ext.commands.context import Context
 from discord.ext import commands
 from dotenv import load_dotenv
-from os import environ
+from os import environ, stat
 from discord_components import DiscordComponents
 
 
 load_dotenv()
 cogs = [music_configuration, about_configuration]
 
+status = discord.Status.online
 activity = discord.Activity(
     name='Music by Sathelo | !help',
     type=discord.ActivityType.listening
 )
 bot = commands.Bot(
     command_prefix='!',
-    activity=activity
+    activity=activity,
+    status=status
 )
 
 
@@ -26,7 +29,7 @@ async def on_ready():
 
 
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(ctx: Context, error):
     if isinstance(error, discord.ext.commands.errors.CommandNotFound):
         await ctx.send("Такой команды не существует, прости 😓")
 
